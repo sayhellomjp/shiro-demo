4028e6f3fdeb93feed26d4f04bc1c23bf2429a34
majunpeng <sayhello_mjp@163.com>
2018-04-15 19:03:51 +0800

    1

---
+++ shirodemo/src/test/java/Demo1.java
@@ -0,0 +1,25 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Demo1 {
+
+    @Test
+    public void testHelloWorld() {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "22");
+
+        subject.login(token);
+
+        Assert.assertTrue(subject.isAuthenticated());
+    }
+
+}
